f1bcbefb1267f0aecf834e586b00ac1bb2de3182
Hwayeon <snw4002@hanmail.net>
2022-05-22 00:09:08 +0900

    add : housedetail 수정중

--- src/components/house/HouseDetail.vue
+++ src/components/house/HouseDetail.vue
@@ -1,10 +1,38 @@
 <template>
   <div>
-    <div>{{ aptDetail[0].apartmentName }}</div>
-    <div v-for="(item, index) in aptDetail" :key="index">
-      <p>{{ item.dealYear }}</p>
-      <p>{{ item.dealAmount }}</p>
-    </div>
+    <b-card class="mb-5 shadow p-4">
+      <b-row>
+        <b-col>
+          <b-card-title>
+            <p>{{ aptDetail[0].apartmentName }}</p>
+            <div class="img_box">
+              <img src="@/assets/aptimg/apt1.jpg" alt="img" />
+            </div>
+          </b-card-title>
+          <b-card-sub-title> </b-card-sub-title>
+          <b-card-text>{{ aptDetail[0].buildYear }}</b-card-text>
+        </b-col>
+        <b-col>
+          <table>
+            <tr>
+              <th>거래일</th>
+              <th>거래가격</th>
+              <th>면적</th>
+              <th>층</th>
+            </tr>
+            <tr v-for="(item, index) in aptDetail" :key="index">
+              <td>
+                {{ item.dealYear }}.{{ item.dealMonth }}.{{ item.dealDay }}
+              </td>
+              <td>{{ item.dealAmount }}</td>
+              <td>{{ item.area }}</td>
+              <td>{{ item.floor }}</td>
+            </tr>
+          </table>
+        </b-col>
+      </b-row>
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
   </div>
 </template>
 
@@ -15,13 +43,49 @@ export default {
   },
   data() {
     return {
-      // aptDetail: [],
+      sortBy: "dealYear",
+      sortDesc: false,
+      fields: [
+        { key: "거래 년도", sortable: true },
+        { key: "거래 가격", sortable: true },
+      ],
+      items: [],
     };
   },
   created() {},
-  computed: {},
+  computed: {
+    // items() {
+    //   items.push();
+    // },
+  },
   watch: {},
 };
 </script>
 
-<style></style>
+<style scoped>
+.card {
+  border-radius: 20px;
+}
+table {
+  width: 100%;
+}
+.card-title p {
+  font-size: 30px;
+  color: #0ca66d;
+}
+.card-title p::after {
+  display: block;
+  content: "";
+  width: 80%;
+  height: 3px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  background-color: #0ca66d;
+}
+.img_box {
+  margin-bottom: 20px;
+}
+.img_box img {
+  width: 60%;
+}
+</style>
